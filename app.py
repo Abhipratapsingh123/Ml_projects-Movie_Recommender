002ba95e7e7cba07d9f291be0c54f725f9d442f5
Abhi pratap singh <138346153+Abhipratapsingh123@users.noreply.github.com>
2025-06-03 06:09:18 +0000

    Changed

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 similarity = cosine_similarity(vectors)
 
 # ttle of the web site
-st.title("NextWatch")
+st.title("Next Watch")
 
 selected_movie_name = st.selectbox('Which movie you want to see', movies['title'].values)
 
