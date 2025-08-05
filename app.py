c8066c701f0fe9159ca386696d4c179a1958319b
Abhi pratap singh <138346153+Abhipratapsingh123@users.noreply.github.com>
2025-08-05 13:24:30 +0000

    poster-viewing-functionality-added

--- app.py
+++ app.py
@@ -3,6 +3,35 @@ import pickle
 import pandas as pd
 import requests
 
+st.set_page_config(page_title="Beta Columns UI", layout="wide")
+
+st.markdown(
+    """
+    <style>
+        .main-title {
+            font-size: 30px;
+            font-weight: bold;
+            margin-bottom: 20px;
+            color: #1F4E79;
+        }
+        .beta-title {
+            font-size: 22px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+        .beta-desc {
+            font-size: 16px;
+            color: #444444;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+
+
 def fetch_poster(movie_id):
     responce= requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=330f75c546b68b44299896d1f44f86b3')
     data= responce.json()
@@ -23,7 +52,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 similarity = cosine_similarity(vectors)
 
 # ttle of the web site
-st.title("Next Watch")
+st.markdown('<div class="Next Watch">Beta Comparison</div>', unsafe_allow_html=True)
 
 selected_movie_name = st.selectbox('Which movie you want to see', movies['title'].values)
 
@@ -44,29 +73,27 @@ def recommend(movie):
 
 
 if st.button("Recommend"):
-    names,posters = recommend(selected_movie_name)
-    col1, col2, col3, col4, col5= st.columns(5)
+    names, posters = recommend(selected_movie_name)
 
-    # Add content to each column
+    # Use spacing between elements and maintain uniform image size
+    col1, col2, col3, col4, col5 = st.columns(5, gap="medium")
+
     with col1:
-        st.text(names[0])
-        st.image(posters[0])
+        st.image(posters[0], use_column_width=True, caption=names[0])
 
     with col2:
-        st.text(names[1])
-        st.image(posters[1])
+        st.image(posters[1], use_column_width=True, caption=names[1])
 
     with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
+        st.image(posters[2], use_column_width=True, caption=names[2])
 
+    with col4:
+        st.image(posters[3], use_column_width=True, caption=names[3])
+
+    with col5:
+        st.image(posters[4], use_column_width=True, caption=names[4])
+
+# Add vertical space before Reset button
+st.markdown("###")
 st.button("Reset", type="primary")
 
