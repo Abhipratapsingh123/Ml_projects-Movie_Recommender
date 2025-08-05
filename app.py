4b68e1617c9515215b0e36f00a172b5ea0666688
Abhi pratap singh <138346153+Abhipratapsingh123@users.noreply.github.com>
2025-08-05 13:04:39 +0000

    poster-viewing-functionality-added

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
+
+def fetch_poster(movie_id):
+    responce= requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=330f75c546b68b44299896d1f44f86b3')
+    data= responce.json()
+    return "https://image.tmdb.org/t/p/w500/"+data["poster_path"]
+
 
 # loading new_df dataset and similarity function
 
@@ -27,15 +34,39 @@ def recommend(movie):
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
+    recommended_movies_poster=[]
     for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-    return recommended_movies
+          # fetching poster from API
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommended_movies,recommended_movies_poster
 
 
 if st.button("Recommend"):
-    recommendations = recommend(selected_movie_name)
-    for i in recommendations:
-        st.write(i)
+    names,posters = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5= st.columns(5)
+
+    # Add content to each column
+    with col1:
+        st.header(names[0])
+        st.write(posters[0])
+
+    with col2:
+        st.header(names[1])
+        st.write(posters[1])
+
+    with col3:
+        st.header(names[2])
+        st.write(posters[2])
+    
+    with col4:
+        st.header(names[3])
+        st.write(posters[3])
+    
+    with col5:
+        st.header(names[4])
+        st.write(posters[4])
 
 st.button("Reset", type="primary")
 
